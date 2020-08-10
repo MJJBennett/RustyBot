cf14b54dbc69c001b0f91d804f8329c411ef4298
Michael Bennett <michaelbennett.dev@gmail.com>
2020-08-10 14:23:27 -0700

    Commit yesterday's work for push.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,12 @@ use async_std::net::{TcpStream};
 use async_std::fs;
 use std::time::Duration;
 use async_std::task;
-use async_std::io::{BufReader, BufWriter};
+use async_std::io::{BufReader, BufWriter, Read};
 use regex::Regex;
 use async_std::prelude::*;
 use async_trait::async_trait;
 use std::sync::Arc;
+use futures::{select};
 
 #[async_trait]
 trait IRCStream {
@@ -43,7 +44,7 @@ struct EasyReader {
 }
 
 struct IRCBotClient {
-    stream: Arc<TcpStream>,
+    stream: TcpStream,
     nick: String,
     secret: String,
     channel: String,
@@ -64,9 +65,9 @@ impl EasyReader {
 impl IRCBotClient {
     async fn connect(nick: String, secret: String, channel: String) -> IRCBotClient {
         // Creates the stream object that will go into the client.
-        let stream = Arc::new(TcpStream::connect("irc.chat.twitch.tv:6667").await.unwrap());
+        let stream = TcpStream::connect("irc.chat.twitch.tv:6667").await.unwrap();
         // Get a stream reference to use for reading.
-        let reader = EasyReader::new(&*Arc::clone(&stream));
+        let reader = EasyReader::new(stream.clone());
         IRCBotClient { 
             stream: stream,
             nick: nick,
@@ -79,30 +80,38 @@ impl IRCBotClient {
     async fn authenticate(&mut self) -> () {
         println!("Writing password...");
         self.stream.send_pass(&self.secret).await;
-        println!("Writing nickname...").await;
+        println!("Writing nickname...");
         self.stream.send_nick(&self.nick).await; 
         println!("Writing join command...");
         self.stream.send_join(&self.channel).await; 
     }
+
+    async fn send(&mut self, msg: String) -> () {
+        self.stream.send(format!("{}", msg));
+    }
+
+    async fn do_command(&mut self, user: String, cmd: String) -> () {
+
+    }
 }
 
 fn get_file_trimmed(filename: &str) -> String {
-    match fs::read_to_string(filename) {
+    match std::fs::read_to_string(filename) {
         Ok(s) => s.trim().to_string(),
         Err(e) => panic!("Could not open file ({}):\n{}", filename, e)
     }
 }
 
-fn main()
-{
+async fn async_main() {
+
     let nick = get_file_trimmed("auth/user.txt");
     let secret = get_file_trimmed("auth/secret.txt");
     let channel = get_file_trimmed("auth/id.txt");
 
     println!("Nick: {} | Secret: {} | Channel: {}", nick, secret, channel);
 
-    let mut client = task::block_on(async {IRCBotClient::connect(nick, secret, channel).await?});
-    client.authenticate();     
+    let mut client: IRCBotClient = IRCBotClient::connect(nick, secret, channel).await;
+    client.authenticate().await;     
 
     println!("Starting loop.");
 
@@ -110,20 +119,31 @@ fn main()
     let priv_re = Regex::new(r":(\w*)!\w*@\w*\.tmi\.twitch\.tv PRIVMSG #\w* :\s*(bot |!|~)\s*(.+?)\s*$").unwrap();
 
     loop {
-        if let Err(e) = client.reader.read_line() {
-            println!("Encountered error: {}", e);
-            continue;
+        select! {
+            line = client.reader.read_line() => match line {
+                Ok(line) => {
+                    let line = &client.reader.line;
+                    println!("[Received] Message: {}", line.trim());
+                    let (name, command) = match priv_re.captures(line.as_str())
+                    {
+                        Some(caps) => (caps.get(1).unwrap().as_str(), caps.get(3).unwrap().as_str()),
+                        None => continue
+                    };
+                    println!("[Parsed Command] Name: {} | Command: '{}'", name, command);
+                    client.do_command(name, command).await;
+                },
+                Err(e) => {
+                    println!("Encountered error: {}", e);
+                    continue;
+                }
+            },
+            _ = task::sleep(Duration::from_secs(1)) =
+
+            task::block_on(async { task::sleep(Duration::from_secs(1)).await; });
         }
-
-        let line = &client.reader.line;
-        println!("[Received] Message: {}", line.trim());
-        let (name, command) = match priv_re.captures(line.as_str())
-        {
-            Some(caps) => (caps.get(1).unwrap().as_str(), caps.get(3).unwrap().as_str()),
-            None => continue
-        };
-        println!("[Parsed Command] Name: {} | Command: '{}'", name, command);
-
-        task::block_on(async { task::sleep(Duration::from_secs(1)).await; });
     }
+}
+
+fn main() {
+    task::block_on(async { async_main().await })
 }
